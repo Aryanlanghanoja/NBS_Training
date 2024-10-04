692808ad806ecaf2152abfc60ec9981a00c982a3
Aryan Langhanoja <115221198+Aryanlanghanoja@users.noreply.github.com>
2024-10-04 11:38:19 +0530

    Learned Templetes

---
+++ 09-30-2024 Linked List/Add.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+template <typename p , typename q>
+
+class MyClass {
+private:
+    p a;
+    q b;
+
+public:
+    MyClass(p x, q y) {
+        cout <<  x + y;
+    }
+};
+
+int main() { 
+    MyClass<int, float> obj(4, 4.5);
+    return 0;
+}
